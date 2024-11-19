5c466a93ce85af565d3d409bb27dd7d71a886812
RaulPalao <r.palaolozano@edu.gva.es>
2024-11-19 09:47:15 +0100

    Añado todos los ficheros

---
+++ src/crearpdf/CrearPDF.java
@@ -0,0 +1,26 @@
+package crearpdf;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.Paragraph;
+import java.io.FileOutputStream;
+
+public class CrearPDF {
+    public void añadirTexto() throws Exception {
+        Document document = new Document();
+        String parrafo = "Nombre: ___________________ ";
+        PdfWriter.getInstance(document, new FileOutputStream("recibo.pdf"));
+        document.open();
+        document.add(new Paragraph(parrafo));
+        document.close();
+    }
+
+    public static void main(String args[]) {
+        try {
+            CrearPDF p = new CrearPDF();
+            p.añadirTexto();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
